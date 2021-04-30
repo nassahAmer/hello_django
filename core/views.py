da67752ad69f8f21df751a652d01f49715c6e588
Hassan <hamerfh.2020@gmail.com>
2021-04-30 15:22:01 -0400

    Commit com nova rota para somar

--- core/views.py
+++ core/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def hello(request, nome):
     return HttpResponse('<h1>Hello World!</h1>\n<h2>Hello {}</h2>'.format(nome))
+
+def soma(request, a, b):
+    soma = int(a) + int(b)
+    return HttpResponse('<h1>Soma de {} e {} : {}</h1>'.format(a,b,soma))
